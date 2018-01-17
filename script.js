d9fe05bb47417d935e04fe5ee02fd382691d65f6
wky0615 <wky_0615@163.com>
2018-01-17 14:25:03 +0800

    更新作品集

---
+++ script.js
@@ -0,0 +1,9 @@
+portfolioAll.onclick = function(){
+  portfolioBar.className = 'footBar state-1'
+}
+portfolioFrame.onclick = function(){
+  portfolioBar.className = 'footBar state-2'
+}
+portfolioJs.onclick = function(){
+  portfolioBar.className = 'footBar state-3'
+}
