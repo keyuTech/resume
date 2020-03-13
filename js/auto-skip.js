b470ff3627ab1fb53aea9c37f9ee00a55921d6d3
wky <wky_0615@163.com>
2020-03-13 13:53:23 +0800

    Remove skip style

--- js/auto-skip.js
+++ js/auto-skip.js
@@ -1,44 +1,47 @@
-!function(){
-  let view = document.querySelector('.menu')
-  let controller = {
-    view: null,
-    init: function(view){
-      this.view = view
-      this.aTags = view.querySelectorAll('li > a')
-      this.bindEvents(this.aTags)
-    },
-    bindEvents: function(aTags){
-      let _this = this
-      for(let i = 0; i < aTags.length; i++){
-        aTags[i].onclick = function(e){
-          e.preventDefault()
-          let href = e.currentTarget.getAttribute('href')
-          let targetElement = document.querySelector(href)
-          _this.top = targetElement.offsetTop
-          _this.addAnimate()
-        }
-      }
-    },
-    addAnimate: function(){
-      let currentTop = window.scrollY
-      let targetTop = this.top - 10
+!function () {
+    let view = document.querySelector('.menu');
+    let controller = {
+        view: null,
+        init: function (view) {
+            this.view = view;
+            this.aTags = view.querySelectorAll('li > a');
+            this.bindEvents(this.aTags);
+        },
+        bindEvents: function (aTags) {
+            let _this = this;
+            for (let i = 0; i < aTags.length; i++) {
+                aTags[i].onclick = function (e) {
+                    e.preventDefault();
+                    let href = e.currentTarget.getAttribute('href');
+                    let targetElement = document.querySelector(href);
+                    _this.top = targetElement.offsetTop;
+                    console.log(targetElement)
+                    console.log(_this.top)
+                    _this.addAnimate();
+                };
+            }
+        },
+        addAnimate: function () {
+            let currentTop = window.scrollY;
+            let targetTop = this.top - 80;
 
-      //引入Tweenjs
-      function animate(time) {
-        requestAnimationFrame(animate)
-        TWEEN.update(time)
-      }
-      requestAnimationFrame(animate)
-      // Setup the animation loop.
-      var coords = { y: currentTop }
-      var tween = new TWEEN.Tween(coords)
-        .to({ y: targetTop }, 1000)
-        .easing(TWEEN.Easing.Quadratic.InOut)
-        .onUpdate(function() {
-          window.scrollTo(0, coords.y)
-        })
-        .start()
-    }
-  }
-  controller.init.call(controller, view)
-}.call()
+            //引入Tweenjs
+            function animate (time) {
+                requestAnimationFrame(animate);
+                TWEEN.update(time);
+            }
+
+            requestAnimationFrame(animate);
+            // Setup the animation loop.
+            var coords = {y: currentTop};
+            var tween = new TWEEN.Tween(coords)
+                .to({y: targetTop}, 1000)
+                .easing(TWEEN.Easing.Quadratic.InOut)
+                .onUpdate(function () {
+                    window.scrollTo(0, coords.y);
+                })
+                .start();
+        }
+    };
+    controller.init.call(controller, view);
+}.call();
